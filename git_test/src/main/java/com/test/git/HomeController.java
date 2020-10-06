52391758d9249aa87e2c32bdaa26c656fb398480
PC-05 <PC-05@DESKTOP-DKGPK52>
2020-10-06 11:18:57 +0900

    modify HomeController : modify date2.getString()

--- git_test/src/main/java/com/test/git/HomeController.java
+++ git_test/src/main/java/com/test/git/HomeController.java
@@ -31,7 +31,10 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", date.getTime()+"" );
+		Date date2 = new Date();
+		
+		
+		model.addAttribute("serverTime", date.getTime()+""+date2 );
 		
 		return "home";
 	}
